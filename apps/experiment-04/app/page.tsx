5076cb1fe31b2c3f5bca9cbfc6ae4fdf5c0b9f96
pasqualevitiello <pasqualevitiello@gmail.com>
2025-03-14 16:02:56 +0100

    mc

--- apps/experiment-04/app/page.tsx
+++ apps/experiment-04/app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset className="overflow-auto px-4 md:px-6 lg:px-8">
-          <header className="bg-sidebar/90 backdrop-blur-sm sticky top-0 z-9999 -mx-2 px-2">
+          <header className="bg-sidebar/90 backdrop-blur-sm sticky top-0 z-50 -mx-2 px-2">
             <div className="flex shrink-0 items-center gap-2 border-b py-4 w-full max-w-7xl mx-auto">
               <div className="flex-1">
                 <div className="relative inline-flex">
@@ -53,22 +53,6 @@ export default function Page() {
             </div>
           </div>
 
-          <div className="grid grid-cols-2 w-48 gap-8 mb-20">
-          <div className="size-20 bg-white shadow-2xs rounded"></div>
-<div className="size-20 bg-white rounded shadow-2xs"></div>
-<div className="size-20 bg-white shadow-xs rounded"></div>
-<div className="size-20 bg-white rounded shadow-[0_1px_1px_rgba(0,0,0,0.02),0_1px_2px_rgba(0,0,0,0.05)]"></div>
-<div className="size-20 bg-white shadow-sm rounded"></div>
-<div className="size-20 bg-white rounded shadow-[0_1px_1px_rgba(0,0,0,0.04),0_2px_2px_rgba(0,0,0,0.04),0_2px_4px_rgba(0,0,0,0.04)]"></div>
-<div className="size-20 bg-white shadow-md rounded"></div>
-<div className="size-20 bg-white rounded shadow-[0_1px_1px_rgba(0,0,0,0.04),0_2px_2px_rgba(0,0,0,0.04),0_4px_4px_rgba(0,0,0,0.04),0_8px_8px_rgba(0,0,0,0.04)]"></div>
-<div className="size-20 bg-white shadow-lg rounded"></div>
-<div className="size-20 bg-white rounded shadow-[0_1px_1px_rgba(0,0,0,0.04),0_2px_2px_rgba(0,0,0,0.04),0_4px_4px_rgba(0,0,0,0.04),0_8px_8px_rgba(0,0,0,0.04),0_16px_16px_rgba(0,0,0,0.04)]"></div>
-<div className="size-20 bg-white shadow-xl rounded"></div>
-<div className="size-20 bg-white rounded shadow-[0_1px_1px_rgba(0,0,0,0.04),0_2px_2px_rgba(0,0,0,0.04),0_4px_4px_rgba(0,0,0,0.04),0_8px_8px_rgba(0,0,0,0.04),0_16px_16px_rgba(0,0,0,0.04),0_32px_32px_rgba(0,0,0,0.04)]"></div>
-<div className="size-20 bg-white shadow-2xl rounded"></div>
-<div className="size-20 bg-white rounded shadow-[0_1px_1px_rgba(0,0,0,0.04),0_2px_2px_rgba(0,0,0,0.04),0_4px_4px_rgba(0,0,0,0.04),0_8px_8px_rgba(0,0,0,0.04),0_16px_16px_rgba(0,0,0,0.04),0_32px_32px_rgba(0,0,0,0.04),0_64px_64px_rgba(0,0,0,0.04)]"></div>
-          </div>
         </SidebarInset>
       </SidebarProvider>
     </div>
