d000e8427ed9aa72a0803b3fb95fd18055ddcdde
pasqualevitiello <pasqualevitiello@gmail.com>
2025-04-02 16:41:09 +0200

    feat: integrate calendar component

--- apps/experiment-06/app/page.tsx
+++ apps/experiment-06/app/page.tsx
@@ -20,6 +20,7 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import ThemeToggle from "@/components/theme-toggle";
+import BigCalendar from "@/components/big-calendar";
 
 export default function Page() {
   return (
@@ -49,12 +50,7 @@ export default function Page() {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-          </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+          <BigCalendar />
         </div>
       </SidebarInset>
     </SidebarProvider>
