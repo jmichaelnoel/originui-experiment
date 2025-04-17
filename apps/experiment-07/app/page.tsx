0129941468d5d35c737a6f85c15b2eea7deedf9a
pasqualevitiello <pasqualevitiello@gmail.com>
2025-04-17 11:34:44 +0200

    chore: init exp 7

---
+++ apps/experiment-07/app/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Experiment 07 - Crafted.is",
+};
+
+export default function Page() {
+  return (
+    <div className="min-h-svh bg-muted">ok</div>
+  );
+}
